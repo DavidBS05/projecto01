dd0ebb1c18385e21b6fbd1853d692d74fa68e449
David <dbs00@iesemilidarder.com>
2018-11-08 20:15:02 +0100

    Commit

--- src/main/java/com/iesemilidarder/dbonnin/projecto01/DataHelper.java
+++ src/main/java/com/iesemilidarder/dbonnin/projecto01/DataHelper.java
@@ -1,10 +1,55 @@
 package com.iesemilidarder.dbonnin.projecto01;
 
-import com.iesemilidarder.dbonnin.projecto01.data.Product;
+import com.iesemilidarder.dbonnin.projecto01.data.*;
+import org.apache.commons.collections4.IteratorUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class DataHelper {
     public static List<Product> items = new ArrayList<>();
+
+    public static List<Product> getData () {
+        doInit();
+        return items;
+    }
+
+    public static void insert (Product product) {
+        items.add(product);
+    }
+
+    private static void doInit() {
+        if (items.isEmpty()){
+            for (int i =0;i<40;i++) {
+                Activity activity = new Activity();
+                activity.setCountry("pais"+i);
+                items.add(activity);
+
+                HotelBooking hotelBooking = new HotelBooking();
+                hotelBooking.setCountry("pais"+i);
+                items.add(hotelBooking);
+
+                Place place = new Place();
+                place.setCountry("pais"+i);
+                items.add(place);
+
+                RestaurantBooking restaurantBooking = new RestaurantBooking();
+                restaurantBooking.setCountry("pais"+i);
+                items.add(restaurantBooking);
+            }
+            for (Product i:items){
+
+            }
+            IteratorUtils.forEach(items.iterator(), input -> {
+                System.out.println(input.getActivityKind());
+                try{
+                    //ejecuta insert into places (name,description) values (input.getName(),input.getDescription()
+                    //commit
+                }catch (Exception e){
+                    //rollback
+                }
+            });
+            System.out.println("Proxima parada!");
+        }
+    }
 }
